73e1ff3d08d9227e9211ccf89ffd527442783d03
ethanharrison <harrem18@wfu.edu>
2019-10-30 22:45:22 -0400

    Comment printing arguments for time. Print unsorted and sorted of both

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -19,7 +19,8 @@ int main() {
     LinkList List; //create new object
     Node *item; //create the item to insert into the list
 
-    for (int j = 0; j < 10000; j = j + 100) {
+    //for (int j = 0; j < 10000; j = j + 100) {
+
         // generate vector and linked list of random integers
         for (int i = 0; i < v.size(); i++) {
             int randValue = rand() % 100; //keep the random value the same for the vector and list
@@ -28,42 +29,42 @@ int main() {
         }
 
         // print out unsorted vector
-        /*cout << "Unsorted Vector: " << endl;
+        cout << "Unsorted Vector: " << endl;
         for (int i = 0; i < v.size(); i++) {
             cout << v[i] << "\t";
         }
-        cout << endl;*/
+        cout << endl;
 
         // binary insertion sort
-        //insertionSort(v, v.size());
+        insertionSort(v, v.size());
 
         // check if sorted
-        /*for (int i = 1; i < v.size(); i++) {
+        for (int i = 1; i < v.size(); i++) {
             assert(v[i - 1] <= v[i]);
-        }*/
+        }
 
         // print out sorted vector
-        /*cout << "Sorted Vector: " << endl;
+        cout << "Sorted Vector: " << endl;
         for (int i = 0; i < v.size(); i++) {
             cout << v[i] << "\t";
         }
-        cout << endl;*/
+        cout << endl;
 
         // print out unsorted linked list
-        /*cout << "Unsorted Linked List: " << endl;
+        cout << "Unsorted Linked List: " << endl;
         List.printList();
-        cout << endl;*/
+        cout << endl;
 
         // insertionSort
-        //List.LLinsertionSort();
+        List.LLinsertionSort();
 
         //print out sorted linked list
-        /*cout << "Sorted Linked List: " << endl;
-        List.printList();*/
+        cout << "Sorted Linked List: " << endl;
+        List.printList();
 
         //cout << endl << endl;
 
-        //measure vector sorting
+        /*//measure vector sorting
         clock_t start_vectorSort = clock(); //start clock
         insertionSort(v, v.size()); // sort vector using Binary Insertion
         clock_t end_vectorSort = clock(); //end clock
@@ -77,7 +78,7 @@ int main() {
         double elapsed_linkListSort =
                 double(end_linkListSort - start_linkListSort) / CLOCKS_PER_SEC; //calculate seconds passed
 
-        cout << elapsed_linkListSort << "\t" << elapsed_vectorSort << endl;
+        cout << j << "\t" << elapsed_linkListSort << "\t" << elapsed_vectorSort << endl;*/
     }
 
-}
+//}
